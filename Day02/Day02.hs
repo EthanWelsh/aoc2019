d607b480312282b938443e7f7229fb1dafcb1f3c
Ethan Welsh <ewelsh@google.com>
2023-12-28 00:05:13 -0500

    Solve day 2 parts 1 and 2

--- Day02/Day02.hs
+++ Day02/Day02.hs
@@ -1,24 +1,100 @@
 module Day02.Day02 (solve) where
 
-import           Utils.ParserUtils     (Parser)
+import           Control.Lens         (element, (.~))
+import           Data.Tuple.Extra     (uncurry3)
 import           Text.Megaparsec
---import Control.Monad (void)
---import Text.Megaparsec.Char (string, char, newline)
+import           Text.Megaparsec.Char (char, newline)
+import           Utils.ParserUtils    (Parser, integer)
 
-type Input = String
+newtype Memory = Memory [Int] deriving (Show)
+newtype Address = Address Int deriving (Show, Num)
+data Instruction = Add Address Address Address
+                 | Multiply Address Address Address
+                 | End deriving (Show)
+type Input = Memory
+
+instructionSize :: Instruction -> Int
+instructionSize (Add _ _ _)      = 4
+instructionSize (Multiply _ _ _) = 4
+instructionSize End              = 1
 
 parseInput :: Parser Input
-parseInput = error "TODO"
+parseInput = do
+   m <- integer `sepBy` char ','
+   return $ Memory m
+
+(@) :: Memory -> Address -> Int
+(@) (Memory m) (Address a) = m !! a
+
+(@+) :: Address -> Int -> Address
+(@+) (Address a) offset = Address $ a + offset
+
+get :: Memory -> Address -> Int
+get m a = m @ a
+
+set :: Memory -> Address -> Int -> Memory
+set (Memory m) (Address a) v = Memory $ (element a .~ v) m
+
+setAll :: Memory -> [(Address, Int)] -> Memory
+setAll = foldl (\m (a, v) -> set m a v)
+
+runInstruction :: Memory -> Instruction -> Memory
+runInstruction m End              = m
+runInstruction m (Add a b r)      = set m r (m @ a + m @ b)
+runInstruction m (Multiply a b r) = set m r (m @ a * m @ b)
+
+setInputs :: Memory -> (Int, Int) -> Memory
+setInputs m (verb, noun) = setAll m [(1, verb), (2, noun)]
+
+next3 :: Memory -> Address -> (Address, Address, Address)
+next3 m a = (Address $ m @ (a @+ 1), Address $ m @ (a @+ 2), Address $ m @ (a @+ 3))
+
+instructionAtAddress :: Memory -> Address -> Instruction
+instructionAtAddress m a = let
+  opcode = m @ a
+  in case opcode of
+    1 -> uncurry3 Add (next3 m a)
+    2 -> uncurry3 Multiply (next3 m a)
+    99 -> End
+
+isEnd :: Instruction -> Bool
+isEnd End = True
+isEnd _ = False
+
+runProgram :: Memory -> Address -> Memory
+runProgram m a = let
+  instruction = instructionAtAddress m a
+  nextA = a @+ instructionSize instruction
+  result = runInstruction m instruction
+  in if isEnd instruction 
+     then m 
+     else runProgram result nextA 
+
+possibleInputs :: [(Int, Int)]
+possibleInputs = [(a, b) | a <- [0..99], b <- [0..99]]
+
+
+runProgramWithInputs :: Memory -> (Int, Int) -> Int
+runProgramWithInputs m inputs = let
+  mm = setInputs m inputs
+  in runProgram mm (Address 0) @ 0
 
 part1 :: Input -> IO ()
 part1 input = do
   putStr "Part 1: "
-  print input
+  let instructions = setInputs input (12, 2)
+  print $ runProgramWithInputs input (12, 2)
+
+findRightInputs :: Memory -> Int -> [(Int, Int)] -> (Int, Int)
+findRightInputs m target (x:xs) = let
+  result = runProgramWithInputs m x
+  in if result == target then x else findRightInputs m target xs
 
 part2 :: Input -> IO ()
 part2 input = do
   putStr "Part 2: "
-  print input
+  let (n, v) = findRightInputs input 19690720 possibleInputs
+  print $ 100 * n + v
 
 solve :: FilePath -> IO ()
 solve filePath = do
