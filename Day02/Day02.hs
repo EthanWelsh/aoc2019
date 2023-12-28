085de4bd561187f1b86f9294049c5f1fcb50ec45
Ethan Welsh <ewelsh@google.com>
2023-12-28 11:05:04 -0800

    Refactor Intcode module into a shareable location

--- Day02/Day02.hs
+++ Day02/Day02.hs
@@ -1,91 +1,27 @@
 module Day02.Day02 (solve) where
 
-import           Control.Lens         (element, (.~))
-import           Data.Tuple.Extra     (uncurry3)
 import           Text.Megaparsec
-import           Text.Megaparsec.Char (char, newline)
+import           Text.Megaparsec.Char (char)
+import           Utils.Intcode
 import           Utils.ParserUtils    (Parser, integer)
 
-newtype Memory = Memory [Int] deriving (Show)
-newtype Address = Address Int deriving (Show, Num)
-data Instruction = Add Address Address Address
-                 | Multiply Address Address Address
-                 | End deriving (Show)
 type Input = Memory
-
-instructionSize :: Instruction -> Int
-instructionSize (Add {})      = 4
-instructionSize (Multiply {}) = 4
-instructionSize End           = 1
 
 parseInput :: Parser Input
 parseInput = do
    m <- integer `sepBy` char ','
-   return $ Memory m
-
-get :: Memory -> Address -> Int
-get m a = m @ a
-
-set :: Memory -> Address -> Int -> Memory
-set (Memory m) (Address a) v = Memory $ (element a .~ v) m
-
-setAll :: Memory -> [(Address, Int)] -> Memory
-setAll = foldl (\m (a, v) -> set m a v)
-
-(@) :: Memory -> Address -> Int
-(@) = get
-
-(@+) :: Address -> Int -> Address
-(@+) (Address a) offset = Address $ a + offset
-
-runInstruction :: Memory -> Instruction -> Memory
-runInstruction m End              = m
-runInstruction m (Add a b r)      = set m r (m @ a + m @ b)
-runInstruction m (Multiply a b r) = set m r (m @ a * m @ b)
-
-setInputs :: Memory -> (Int, Int) -> Memory
-setInputs m (verb, noun) = setAll m [(1, verb), (2, noun)]
-
-next3 :: Memory -> Address -> (Address, Address, Address)
-next3 m a = (Address $ m @ (a @+ 1), Address $ m @ (a @+ 2), Address $ m @ (a @+ 3))
-
-instructionAtAddress :: Memory -> Address -> Instruction
-instructionAtAddress m a = let
-  opcode = m @ a
-  in case opcode of
-    1  -> uncurry3 Add (next3 m a)
-    2  -> uncurry3 Multiply (next3 m a)
-    99 -> End
-
-isEnd :: Instruction -> Bool
-isEnd End = True
-isEnd _   = False
-
-runProgram :: Memory -> Address -> Memory
-runProgram m a = let
-  instruction = instructionAtAddress m a
-  nextA = a @+ instructionSize instruction
-  result = runInstruction m instruction
-  in if isEnd instruction
-     then m
-     else runProgram result nextA
+   return $ memoryFromList m
 
 possibleInputs :: [(Int, Int)]
 possibleInputs = [(a, b) | a <- [0..99], b <- [0..99]]
 
-
-runProgramWithInputs :: Memory -> (Int, Int) -> Int
-runProgramWithInputs m inputs = let
-  mm = setInputs m inputs
-  in runProgram mm (Address 0) @ 0
-
 part1 :: Input -> IO ()
 part1 input = do
   putStr "Part 1: "
-  let instructions = setInputs input (12, 2)
   print $ runProgramWithInputs input (12, 2)
 
 findRightInputs :: Memory -> Int -> [(Int, Int)] -> (Int, Int)
+findRightInputs _ _ [] = error "Reached end of inputs before finding a match"
 findRightInputs m target (x:xs) = let
   result = runProgramWithInputs m x
   in if result == target then x else findRightInputs m target xs
--- Utils/Intcode.hs
+++ Utils/Intcode.hs
@@ -1,2 +1,90 @@
-module Intcode where
+module Utils.Intcode(
+  Memory,
+  Address,
+  Instruction,
+  address,
+  memoryFromList,
+  get,
+  set,
+  setAll,
+  (@),
+  (@+),
+  runInstruction,
+  setInputs,
+  instructionAtAddress,
+  runProgram,
+  runProgramWithInputs
+) where
 
+import           Control.Lens         (element, (.~))
+import           Data.Tuple.Extra     (uncurry3)
+
+newtype Memory = Memory [Int] deriving (Show)
+newtype Address = Address Int deriving (Show, Num)
+data Instruction = Add Address Address Address
+                 | Multiply Address Address Address
+                 | End deriving (Show)
+
+memoryFromList :: [Int] -> Memory
+memoryFromList = Memory
+
+address :: Int -> Address
+address n = Address n
+
+instructionSize :: Instruction -> Int
+instructionSize (Add {})      = 4
+instructionSize (Multiply {}) = 4
+instructionSize End           = 1
+
+get :: Memory -> Address -> Int
+get (Memory m) (Address a) = m !! a
+
+set :: Memory -> Address -> Int -> Memory
+set (Memory m) (Address a) v = Memory $ (element a .~ v) m
+
+setAll :: Memory -> [(Address, Int)] -> Memory
+setAll = foldl (\m (a, v) -> set m a v)
+
+(@) :: Memory -> Address -> Int
+(@) = get
+
+(@+) :: Address -> Int -> Address
+(@+) (Address a) offset = Address $ a + offset
+
+runInstruction :: Memory -> Instruction -> Memory
+runInstruction m End              = m
+runInstruction m (Add a b r)      = set m r (m @ a + m @ b)
+runInstruction m (Multiply a b r) = set m r (m @ a * m @ b)
+
+setInputs :: Memory -> (Int, Int) -> Memory
+setInputs m (verb, noun) = setAll m [(1, verb), (2, noun)]
+
+next3 :: Memory -> Address -> (Address, Address, Address)
+next3 m a = (Address $ m @ (a @+ 1), Address $ m @ (a @+ 2), Address $ m @ (a @+ 3))
+
+instructionAtAddress :: Memory -> Address -> Instruction
+instructionAtAddress m a = let
+  opcode = m @ a
+  in case opcode of
+    1  -> uncurry3 Add (next3 m a)
+    2  -> uncurry3 Multiply (next3 m a)
+    99 -> End
+    x -> error $ "Unexpected opcode=" ++ show x
+
+isEnd :: Instruction -> Bool
+isEnd End = True
+isEnd _   = False
+
+runProgram :: Memory -> Address -> Memory
+runProgram m a = let
+  instruction = instructionAtAddress m a
+  nextA = a @+ instructionSize instruction
+  result = runInstruction m instruction
+  in if isEnd instruction
+     then m
+     else runProgram result nextA
+
+runProgramWithInputs :: Memory -> (Int, Int) -> Int
+runProgramWithInputs m inputs = let
+  mm = setInputs m inputs
+  in runProgram mm (Address 0) @ 0
