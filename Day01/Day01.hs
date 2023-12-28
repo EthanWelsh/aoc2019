84e63d3849083b0f5ad308ec3d021fb531fa8d43
Ethan Welsh <ewelsh@google.com>
2023-12-27 21:53:55 -0500

    Solve day 1 part 1 and 2

--- Day01/Day01.hs
+++ Day01/Day01.hs
@@ -1,24 +1,34 @@
 module Day01.Day01 (solve) where
 
 import           Text.Megaparsec
-import           Utils.ParserUtils (Parser)
+import           Utils.ParserUtils (Parser, integer)
 --import Control.Monad (void)
 --import Text.Megaparsec.Char (string, char, newline)
 
-type Input = String
+type Input = [Int]
 
 parseInput :: Parser Input
-parseInput = error "TODO"
+parseInput = many integer
+
+fuelRequired :: Int -> Int
+fuelRequired m = div m 3 - 2
 
 part1 :: Input -> IO ()
 part1 input = do
   putStr "Part 1: "
-  print input
+  print $ sum $ map fuelRequired input
+
+fuelRequiredPt2 :: Int -> Int
+fuelRequiredPt2 m
+  | n <= 0 = 0
+  | otherwise = n + fuelRequiredPt2 n
+  where
+    n = fuelRequired m
 
 part2 :: Input -> IO ()
 part2 input = do
   putStr "Part 2: "
-  print input
+  print $ sum $ map fuelRequiredPt2 input
 
 solve :: FilePath -> IO ()
 solve filePath = do
